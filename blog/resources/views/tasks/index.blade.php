ac584a97c8e0ca1d6e380e4d681ff0f67574c690
maria <gomenjukmarija@gmail.com>
2017-11-28 21:22:21 +0200

    Working With the Query Builde

--- blog/resources/views/tasks/index.blade.php
+++ blog/resources/views/tasks/index.blade.php
@@ -7,7 +7,12 @@
 <body>
     <ul>
         @foreach ($tasks as $task)
-            <li>{{ $task }}</li>
+
+            <li>
+            	<a href="/tasks/{{ $task->id }}">
+            		{{ $task->body }}
+            	</a>
+            </li>
         @endforeach    
     </ul>
 </body>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/tasks', function () {
+	$tasks = DB::table('tasks')->latest()->get();
+    return view('tasks.index', compact('tasks'));
+});
 
-Route::get('/', function () {
-	$tasks = [
-		'Go to the store',
-		'Finish my screencast',
-		'Clean the house'
-	];
-    return view('welcome', compact('tasks'));
+Route::get('/tasks/{task}', function ($id) {
+	$task = DB::table('tasks')->find($id);
+    return view('tasks.show', compact('task'));
 });
