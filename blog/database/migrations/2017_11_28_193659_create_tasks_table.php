2fe0032c13529f469f2439bb6a110c88324eb9d8
maria <gomenjukmarija@gmail.com>
2017-11-28 22:02:46 +0200

    Eloquent 101

--- blog/database/migrations/2017_11_28_193659_create_tasks_table.php
+++ blog/database/migrations/2017_11_28_193659_create_tasks_table.php
@@ -13,9 +13,10 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-       Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->text('body');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
