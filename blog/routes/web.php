428bde335051b04c4136cbcc5561f363d79022f9
maria <gomenjukmarija@gmail.com>
2017-11-29 23:11:54 +0200

    Layouts and Structure

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,4 +1,4 @@
 <?php
 
-Route::get('/tasks', 'TaskController@index');
-Route::get('/tasks/{task}', 'TaskController@show');
+Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
