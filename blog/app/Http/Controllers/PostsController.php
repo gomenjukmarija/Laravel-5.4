138e137350d2eba75c92994d0497500c60bc68ce
maria <gomenjukmarija@gmail.com>
2017-11-30 23:02:55 +0200

    Form Request Data and CSRF

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -15,4 +16,29 @@ class PostsController extends Controller
     {
     	return view('posts.show');
     }
+
+    public function create ()
+    {
+    	return view('posts.create');
+    }
+
+    public function store ()
+    {
+    	//Create a new post using the request data
+    	//$post = new Post;
+
+       //Save to database
+    	// $post->title = request('title');
+    	// $post->body = request('body');    	
+    	// $post->save();
+
+    	Post::create([
+    			'title' => request('title'),
+    			'body' => request('body')
+    	]);
+
+
+    	//and then redirect to homepage
+    	return redirect('/');
+    }   
 }
