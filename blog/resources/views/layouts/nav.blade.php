60a3a4cbc460e2ab0b2c6ea4212a95a700df6579
gomenjukmarija <gomenjukmarija@gmail.com>
2017-12-05 12:41:19 +0200

    Rapid Authentication and Configuration

--- blog/resources/views/layouts/nav.blade.php
+++ blog/resources/views/layouts/nav.blade.php
@@ -2,11 +2,9 @@
       <div class="blog-masthead">
         <div class="container">
           <nav class="nav">
-            <a class="nav-link active" href="#">Home</a>
-            <a class="nav-link" href="#">New features</a>
-            <a class="nav-link" href="#">Press</a>
-            <a class="nav-link" href="#">New hires</a>
-            <a class="nav-link" href="#">About</a>
+            <a class="nav-link" href="/home">Home</a>
+            <a class="nav-link" href="/login">Login</a>
+            <a class="nav-link" href="/register">Register</a>
           </nav>
         </div>
       </div>
